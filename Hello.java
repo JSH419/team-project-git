56ef854a8d4a90fff07a2ca925642be9cc762216
JSH419 <tmdgh419@nate.com>
2023-05-24 14:20:34 +0900

    Hello.java modify

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.print("Hello Git")
-		System.out.print("Hello Git")
-		System.out.print("That's DVCS!")
+		System.out.print("Hello Git");
+		System.out.print("Hello Git");
+		System.out.print("That's DVCS!");
+		System.out.print("Hi, Hello, Anyoung? check commit history!");
 	}
+}
